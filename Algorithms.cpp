98549c36f92cce526a21d23186f4ac6852f8ed91
YaffyKoyo <qyaffy@gmail.com>
2016-05-06 00:25:51 +0100

    not sure what changed

--- Algorithms.cpp
+++ Algorithms.cpp
@@ -34,6 +34,10 @@ int contiguousSum(int size, int * array);
 
 int vectorMaxSum(int ar_length, vector<int*> intVector);
 
+void quickSort(vector<int>& A, int p, int q);
+
+int partition(vector<int>& A, int p, int q);
+
 
 int main()
 {
@@ -61,44 +65,44 @@ int main()
 
 
 	//Binary tree implement
-/*	BSTnode* bstNode = NULL;
-	srand(time(NULL));
+	/*	BSTnode* bstNode = NULL;
+		srand(time(NULL));
 
-	TreeManipulate *tm = new TreeManipulate();
-
-
-	int a[10];
-	for (int i = 0; i < sizeof(a)/sizeof(a[0]); i++) {
-		a[i] = i;
-	}
-
-	for (int i = 0; i < sizeof(a) / sizeof(a[0]); i++) {
-		int num = rand() % (sizeof(a) / sizeof(a[0])-i) + i;
-		std::swap(a[i], a[num]);
-	}
+		TreeManipulate *tm = new TreeManipulate();
 
 
-	for (int i = 0; i < sizeof(a) / sizeof(a[0]); i++) {
-		bstNode = CreateBST(bstNode, a[i]);
-		tm->Add(a[i]);
-		cout << a[i]<<" ";
-	}
-	cout << endl;
+		int a[10];
+		for (int i = 0; i < sizeof(a)/sizeof(a[0]); i++) {
+			a[i] = i;
+		}
+
+		for (int i = 0; i < sizeof(a) / sizeof(a[0]); i++) {
+			int num = rand() % (sizeof(a) / sizeof(a[0])-i) + i;
+			std::swap(a[i], a[num]);
+		}
 
 
-	Inorder(bstNode);
-	cout << endl;
-	tm->InOrderTraversal(tm->root);
-	cout << endl;
-	tm->PreOrderTraversal(tm->root);
-	cout << endl;
-	tm->PostOrderTraversal(tm->root);*/
+		for (int i = 0; i < sizeof(a) / sizeof(a[0]); i++) {
+			bstNode = CreateBST(bstNode, a[i]);
+			tm->Add(a[i]);
+			cout << a[i]<<" ";
+		}
+		cout << endl;
+
+
+		Inorder(bstNode);
+		cout << endl;
+		tm->InOrderTraversal(tm->root);
+		cout << endl;
+		tm->PreOrderTraversal(tm->root);
+		cout << endl;
+		tm->PostOrderTraversal(tm->root);*/
 
 	char* string1 = "string1";
 	char string2[] = "string2";
 	cout << string1 << endl;
 
-	//test.............for hashMap
+//test.............for hashMap
 	/*int num;
 	cout << "type: ";
 	cin >> num;
@@ -126,48 +130,48 @@ int main()
 */
 
 //test for sherlock and anagram
-/*int num;
-cin >> num;
-for (int i = 0; i < num; i++) {
-	string temp;
-	cin >> temp;
-	int length = temp.length();
-	int sum = 0;
-	for (int subLength = 1; subLength < length;subLength++) {
-		vector<string> subStrings;
-		for (int n=0;n<length-subLength+1;n++)
-		{
-			subStrings.push_back(temp.substr(n, subLength));
-		}
-		cout << "sub string length = " << subLength <<":"<< endl;
-		for (auto it = subStrings.begin();it!=subStrings.end()-1;++it)
-		{
-			for (auto itNext = it+1;itNext!=subStrings.end();++itNext)
+	/*int num;
+	cin >> num;
+	for (int i = 0; i < num; i++) {
+		string temp;
+		cin >> temp;
+		int length = temp.length();
+		int sum = 0;
+		for (int subLength = 1; subLength < length;subLength++) {
+			vector<string> subStrings;
+			for (int n=0;n<length-subLength+1;n++)
 			{
-				if (isAnagram(*it,*itNext))
+				subStrings.push_back(temp.substr(n, subLength));
+			}
+			cout << "sub string length = " << subLength <<":"<< endl;
+			for (auto it = subStrings.begin();it!=subStrings.end()-1;++it)
+			{
+				for (auto itNext = it+1;itNext!=subStrings.end();++itNext)
 				{
-					sum++;
+					if (isAnagram(*it,*itNext))
+					{
+						sum++;
+					}
 				}
 			}
+			cout << endl;
+
 		}
-		cout << endl;
+		cout << sum << endl;
 
-	}
-	cout << sum << endl;
-
-}*/
+	}*/
 
 //test for insertion sort part1
-/*vector <int>  _ar;
-int _ar_size;
-cin >> _ar_size;
-for (int _ar_i = 0; _ar_i < _ar_size; _ar_i++) {
-	int _ar_tmp;
-	cin >> _ar_tmp;
-	_ar.push_back(_ar_tmp);
-}
+	/*vector <int>  _ar;
+	int _ar_size;
+	cin >> _ar_size;
+	for (int _ar_i = 0; _ar_i < _ar_size; _ar_i++) {
+		int _ar_tmp;
+		cin >> _ar_tmp;
+		_ar.push_back(_ar_tmp);
+	}
 
-insertionSort(_ar);*/
+	insertionSort(_ar);*/
 
 
 //test for insertion sort part2
@@ -186,17 +190,35 @@ insertionSort(_ar);*/
 	delete _ar;*/
 
 //test for maxsubarray
-	int num;
-	cin >> num;
-	for (int i = 0; i < num; i++) {
-		int n, *array;
-		cin >> n;
-		array = new int[n];
-		for (int j = 0; j < n; j++) {
-			cin >> array[j];
-		}
-		cout << contiguousSum(n, array) << endl;
-	}
+	/*int num;
+		cin >> num;
+		for (int i = 0; i < num; i++) {
+			int n, *array;
+			cin >> n;
+			array = new int[n];
+			for (int j = 0; j < n; j++) {
+				cin >> array[j];
+			}
+			cout << contiguousSum(n, array) << endl;
+		}*/
+
+//test for quicksort
+
+	vector<int> A = { 6,10,13,5,8,3,2,25,4,11 };
+	int p = 0;
+	int q = 10;
+
+	cout << "======Original=======" << endl;
+	for (auto e : A)
+		cout << e << " ";
+	cout << endl;
+
+	quickSort(A, p, q);
+
+	cout << "======Sorted=======" << endl;
+	for (auto e : A)
+		cout << e << " ";
+	cout << endl;
 
 	system("pause");
 	return 0;
@@ -357,7 +379,7 @@ void insertionSort(int ar_size, int* ar) {
 		{
 			cout << ar[num] << ' ';
 		}
-		cout <<endl<<"j loop times: "<<count<< endl;
+		cout << endl << "j loop times: " << count << endl;
 		count_i++;
 	}
 	cout << "i loop times: " << count_i << endl;
@@ -369,26 +391,26 @@ int nonContiguousSum(int size, int *array) {
 	int sum = 0;
 	bool *flags;
 	flags = new bool[size];
-	for (int i=0;i<size;i++)
+	for (int i = 0; i < size; i++)
 	{
 		flags[i] = false;
-		if (array[i]>0)
+		if (array[i] > 0)
 		{
 			flags[i] = true;
 			postiveNum++;
 		}
-		if (maxNum<array[i])
+		if (maxNum < array[i])
 		{
 			maxNum = array[i];
 		}
 	}
 
-	if (postiveNum==0)
+	if (postiveNum == 0)
 	{
 		sum = maxNum;
 	}
 	else {
-		for (int i=0;i<size;i++)
+		for (int i = 0; i < size; i++)
 		{
 			if (flags[i])
 			{
@@ -402,28 +424,28 @@ int nonContiguousSum(int size, int *array) {
 int contiguousSum(int size, int *array) {
 	int sum = 0;
 	int max = -100000;
-	for (int subLength = 1; subLength < size+1; subLength++) {
+	for (int subLength = 1; subLength < size + 1; subLength++) {
 		vector<int*> subArrays;
 		int currentLengthMaxSum = 0;
 		for (int n = 0; n < size - subLength + 1; n++) {
 			int* subArray;
 			subArray = new int[subLength];
-			for (int k=0;k<subLength;k++)
+			for (int k = 0; k < subLength; k++)
 			{
 				subArray[k] = array[n + k];
 			}
 			subArrays.push_back(subArray);
 		}
-		for (auto it = subArrays.begin();it!=subArrays.end();++it)
+		for (auto it = subArrays.begin(); it != subArrays.end(); ++it)
 		{
 			for (size_t i = 0; i < subLength; i++)
 			{
-				cout << (*it)[i]<<" ";
+				cout << (*it)[i] << " ";
 			}
 			cout << endl;
 		}
 		currentLengthMaxSum = vectorMaxSum(subLength, subArrays);
-		if (max<currentLengthMaxSum)
+		if (max < currentLengthMaxSum)
 		{
 			max = currentLengthMaxSum;
 		}
@@ -440,10 +462,42 @@ int vectorMaxSum(int ar_length, vector<int*> intVector) {
 		{
 			tempSum += (*it)[n];
 		}
-		if (sum<tempSum)
+		if (sum < tempSum)
 		{
 			sum = tempSum;
 		}
 	}
 	return sum;
+}
+
+
+void quickSort(vector<int>& A, int p, int q)
+{
+	int r;
+	if (p < q)
+	{
+		r = partition(A, p, q);
+		quickSort(A, p, r);
+		quickSort(A, r + 1, q);
+	}
+}
+
+int partition(vector<int>& A, int p, int q)
+{
+	int x = A[p];
+	int i = p;
+	int j;
+
+	for (j = p + 1; j < q; j++)
+	{
+		if (A[j] <= x)
+		{
+			i = i + 1;
+			swap(A[i], A[j]);
+		}
+
+	}
+
+	swap(A[i], A[p]);
+	return i;
 }
